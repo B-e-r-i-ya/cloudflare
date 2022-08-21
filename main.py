caab7ad0e4e88c2b503827877e88ffbf444d2a71
Александр <60294062+B-e-r-i-ya@users.noreply.github.com>
2022-08-21 23:42:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,22 @@
-import requests
-import json
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath('..'))
+import CloudFlare
 
-class cf_class:
-    TOKEN = ''
-    EMAIL = ''
+TOKEN = 'cca03865f623bec447d23db4755789a9d544c'
+EMAIL = 'hramovalexandr1988@yandex.ru'
 
-    def __init__(self, email, token):
-        self.TOKEN = token
-        self.EMAIL = email
-
-    def request(self, urls, type):
-        url = 'https://api.cloudflare.com/client/v4/' + urls
-        payload = {}
-        headers = {
-            'X-Auth-Email': self.EMAIL,
-            'X-Auth-Key': self.TOKEN,
-            'content-type': 'application/json'
-        }
-        respose = requests.get(url, data=json.dumps(payload), headers=headers)
-        return respose
-
-    def create_site(self, name):
-        url = 'https://api.cloudflare.com/client/v4/zones'
-        payload = {"name":name,"account":{"id":"0260e75b414a8881dd19d936a2fe6e9d"},"jump_start":True,"type":"full"}
-        headers = {
-            'X-Auth-Email': self.EMAIL,
-            'X-Auth-Key': self.TOKEN,
-            'content-type': 'application/json'
-        }
-        respose = requests.post(url, data=json.dumps(payload), headers=headers)
-        return respose
-
-    def delete_site(self, id):
-        url = 'https://api.cloudflare.com/client/v4/zones/'+id
-        payload = {}
-        headers = {
-            'X-Auth-Email': self.EMAIL,
-            'X-Auth-Key': self.TOKEN,
-            'content-type': 'application/json'
-        }
-        respose = requests.delete(url, data=json.dumps(payload), headers=headers)
-        return respose
-
-cf = cf_class('hramovalexandr1988@yandex.ru', 'cca03865f623bec447d23db4755789a9d544c' )
-#print(cf.delete_site('5e63190dbe5762c3ae072921a14d0811').json())
-print(cf.create_site('777-777.org').json())
+def main():
+    cf = CloudFlare.CloudFlare(EMAIL, TOKEN)
+    zone_name = '777-777.org'
+    #print(cf.zones.post(data={'jump_start': , 'name': "777-777.org"})) #Создание зоны
+    #print(cf.zones.get()) # Просмотр
+    #for i in cf.api_list(): print(i)
+    #for i in cf.zones.dns_records.get('5e63190dbe5762c3ae072921a14d0811'):
+    #    print(cf.zones.dns_records.delete('5e63190dbe5762c3ae072921a14d0811', i['id']))
+    #print(cf.accounts())
+    #print(cf.accounts.rules.lists('0260e75b414a8881dd19d936a2fe6e9d'))
+    print(cf.zones.firewall.rules.delete('5e63190dbe5762c3ae072921a14d0811', '04cc5bef8f054c408df481b43ee94a73'))
+if __name__ == '__main__':
+    main()
