947d6fb3465d55835d8cb0eb798e9f980884d05d
Alexandr <CnbktnV@gmail.com>
2022-08-28 11:08:36 +0700

    init123

--- cloudflare.py
+++ cloudflare.py
@@ -29,19 +29,64 @@ def create_dns(cf, zone_id, data):
 
 def create_firewallrules(cf, zone_id, data):
     filters = cf.zones.filters(zone_id)
-    rules = cf.zones.rules(zone_id)
+    print(filters)
+    rules = cf.zones.firewall.rules.get(zone_id)
     for item_data in data:
+        print(item_data)
+        if filters == []:
+            try:
+                filter_id = cf.zones.filters.post(zone_id, data=[{
+                    "paused": False,
+                    "expression": item_data["filter"]
+                }])["id"]
+            except CloudFlare.exceptions.CloudFlareAPIError as e:
+                print(e)
         for item_filter in filters:
-            if item_data["filter"] != item_filter["result"]["expression"]:
+            print(item_filter)
+            if item_data["filter"] != item_filter["expression"]:
+                print("Создаем фильтр " + item_data["filter"])
+                print(type(item_data["filter"]))
                 try:
-                    cf.zones.filters.post(zone_id, data=[{
+                    filter_id = cf.zones.filters.post(zone_id, data=[{
                                                             "paused": False,
                                                             "expression": item_data["filter"]
-                                                        }])
+                                                        }])["id"]
                 except CloudFlare.exceptions.CloudFlareAPIError as e:
                     print(e)
+            else:
+                print("Фильтр " + item_data["filter"] + " уже есть!")
+                filter_id = item_filter["id"]
+        if rules == []:
+            try:
+                cf.zones.firewall.rules.post(
+                    zone_id, data=[{"action": item_data["action"],
+                                    "priority": item_data["priority"],
+                                    "paused": item_data["paused"],
+                                    "description": item_data["name"],
+                                    "filter":
+                                        {
+                                            "id": filter_id,
+                                            "paused": item_data["paused"],
+                                        }}]
+                )
+            except CloudFlare.exceptions.CloudFlareAPIError as e:
+                print(e)
         for item_rule in rules:
-            if item_data["name"] != item_rule["result"]["description"]:
+            if item_data["name"] != item_rule["description"]:
+                try:
+                    cf.zones.firewall.rules.post(
+                        zone_id, data=[{"action": item_data["action"],
+                                        "priority": item_data["priority"],
+                                        "paused": item_data["paused"],
+                                        "description": item_data["name"],
+                                        "filter":
+                                            {
+                                                "id": filter_id,
+                                                "paused": item_data["paused"],
+                                            }}]
+                    )
+                except CloudFlare.exceptions.CloudFlareAPIError as e:
+                    print(e)
 
 
 cf = CloudFlare.CloudFlare(token=TOKEN)
@@ -62,9 +107,10 @@ dns = [{"name": "@", "type": "A", "content": "8.13.56.100", "proxied": True}]
 
 firewall_rules = [
         {"name": "Block DDoS UserAgent", "action": "block", "filter": "(http.user_agent contains \"Python\")",
-         "priority": 1},
-        {"name": "Allow Enterra list", "action": "allow", "filter": "(ip.src in $white_list_enterra)", "priority": 2},
-        {"name": "Block ne MN", "action": "block", "filter": "(ip.geoip.country ne \"MN\")", "priority": 3}
+         "priority": 1, "paused": False},
+        {"name": "Allow Enterra list", "action": "allow", "filter": "(ip.src in $white_list_enterra)", "priority": 2, "paused": False},
+        {"name": "Block ne MN", "action": "block", "filter": "(ip.geoip.country ne \"MN\")", "priority": 3, "paused": False}
     ]
 
 #create_dns(cf, create_zone(cf, '777-777.org'), dns)
+create_firewallrules(cf, create_zone(cf, '777-777.org'), firewall_rules)
