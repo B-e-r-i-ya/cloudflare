2c9f4f0097917edb83c84074900bf87a75bd3ad5
Alexandr <CnbktnV@gmail.com>
2022-08-28 21:21:31 +0700

    Готовое решение для HTML5 клиентов

--- cloudflare.py
+++ cloudflare.py
@@ -1,4 +1,6 @@
+import ast
 import CloudFlare
+import click
 
 TOKEN = 'wgKC7ELJaC8t6c6DkOttyQEr0bBZV5IQ1Cy8wKtd'
 
@@ -38,7 +40,7 @@ def create_firewallrules(cf, zone_id, data):
                 filter_id = cf.zones.filters.post(zone_id, data=[{
                     "paused": False,
                     "expression": item_data["filter"]
-                }])["id"]
+                }])[0]["id"]
             except CloudFlare.exceptions.CloudFlareAPIError as e:
                 print(e)
         for item_filter in filters:
@@ -50,7 +52,7 @@ def create_firewallrules(cf, zone_id, data):
                     filter_id = cf.zones.filters.post(zone_id, data=[{
                                                             "paused": False,
                                                             "expression": item_data["filter"]
-                                                        }])["id"]
+                                                        }])[0]["id"]
                 except CloudFlare.exceptions.CloudFlareAPIError as e:
                     print(e)
             else:
@@ -113,4 +115,91 @@ firewall_rules = [
     ]
 
 #create_dns(cf, create_zone(cf, '777-777.org'), dns)
-create_firewallrules(cf, create_zone(cf, '777-777.org'), firewall_rules)
+#create_firewallrules(cf, create_zone(cf, '777-777.org'), firewall_rules)
+
+
+@click.command()
+@click.option(
+    '--api_key', '-a',
+    help='Апи токен доступа CF(НЕ Global API Key, а индивидуально созданный)',
+)
+@click.option(
+    '--file', '-f',
+    help='файл для парсинга значений для добавления',
+)
+
+@click.option(
+    '--jenkins', '-j',
+    help='Если скрипт запускается с Jenkins',
+)
+
+
+def main(api_key, file, jenkins):
+    """
+    Данная программа предназначена для автоматизации работы с CloudFlare
+
+    """
+
+
+    firewall_rules = [
+        {"name": "Block DDoS UserAgent", "action": "block", "filter": "(http.user_agent contains \"Python\")",
+         "priority": 1, "paused": False},
+        {"name": "Allow Enterra list", "action": "allow", "filter": "(ip.src in $white_list_enterra)", "priority": 2,
+         "paused": False},
+        {"name": "Block ne MN", "action": "block", "filter": "(ip.geoip.country ne \"MN\")", "priority": 3,
+         "paused": False}
+    ]
+    try:
+        api_key
+    except NameError:
+        api_key = None
+
+    try:
+        file
+    except NameError:
+        file = None
+
+    try:
+        jenkins
+    except NameError:
+        jenkins = None
+
+
+    if not(api_key == None):
+        TOKEN = api_key
+
+    #if file != None:
+     #   file_pars(file)
+
+    if jenkins != None:
+        jenkins = ast.literal_eval(jenkins)
+        dns = {"name": "@", "type": "A", "proxied": True}
+        print(jenkins["mongol"])
+        if jenkins["mongol"] == False:
+            firewall_rules = [
+                {"name": "Block DDoS UserAgent", "action": "block", "filter": "(http.user_agent contains \"Python\")",
+                 "priority": 1, "paused": False},
+                {"name": "Allow Enterra list", "action": "allow", "filter": "(ip.src in $white_list_enterra)",
+                 "priority": 2,
+                 "paused": False},
+                {"name": "Block ne MN", "action": "block", "filter": "(ip.geoip.country ne \"MN\")", "priority": 3,
+                 "paused": False}
+            ]
+        elif jenkins["mongol"] == True:
+            firewall_rules = [
+                {"name": "Block DDoS UserAgent", "action": "block", "filter": "(http.user_agent contains \"Python\")",
+                 "priority": 1, "paused": False},
+                {"name": "Allow Enterra list", "action": "allow", "filter": "(ip.src in $white_list_enterra)",
+                 "priority": 2,
+                 "paused": False},
+                {"name": "Block ne MN", "action": "block", "filter": "(ip.geoip.country ne \"MN\")", "priority": 3,
+                 "paused": True}
+            ]
+
+        zone_id = create_zone(cf, jenkins["zone"])
+        dns["content"] = jenkins["ipaddress"]
+        create_dns(cf, zone_id, dns)
+        create_firewallrules(cf, zone_id, firewall_rules)
+
+if __name__ == "__main__":
+    main()
